7ad5dc6773a027cb2b56589918030fec9123d2ea
Cypher <mrcyphernp@gmail.com>
2022-12-24 17:46:37 +0545

    Completed anime birthday remainder

---
+++ 01-birthday-reminder/setup/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Luffy',
+    age: 19,
+    image:
+      'https://i.pinimg.com/originals/50/08/ef/5008efb9df96969624d2674645027a3a.png',
+  },
+  {
+    id: 2,
+    name: 'Sasuke',
+    age: 21,
+    image:
+      'https://i.pinimg.com/originals/4a/50/e9/4a50e93c205e7f14ea5385fb792d1e49.png',
+  },
+  {
+    id: 3,
+    name: 'Zoro',
+    age: 22,
+    image:
+      'https://i.pinimg.com/736x/65/23/43/652343e839c6621c1c4739b89c218948.jpg',
+  },
+  {
+    id: 4,
+    name: 'Itachi',
+    age: 34,
+    image:
+      'http://pm1.narvii.com/7655/a7eae547bcca2c966862c57bee767208f0bc1267r1-1080-1920v2_00.jpg',
+  },
+  {
+    id: 5,
+    name: 'Sanji',
+    age: 29,
+    image:
+      'https://i.pinimg.com/736x/e9/6f/83/e96f83a6a8c3c859a362470e8abe065f.jpg',
+  },
+];
